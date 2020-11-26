960d7b47643d6c87befdf0f0dfac05135a09d8db
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-11-26 00:30:58 -0300

    Create UserController with a POST method to create an user

---
+++ src/main/kotlin/com/usp/holdinghands/controllers/UserController.kt
@@ -0,0 +1,42 @@
+package com.usp.holdinghands.controllers
+
+import com.usp.holdinghands.models.HelpType
+import com.usp.holdinghands.models.User
+import com.usp.holdinghands.models.UserRequest
+import com.usp.holdinghands.repositories.UserRepository
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/api/user")
+class UserController(val userRepository: UserRepository) {
+
+    @PostMapping("/create")
+    fun createUser(@RequestBody userRequest: UserRequest): User {
+        val user = User(
+                name = userRequest.name,
+                helpTypes = convertToDatabaseColumn(userRequest.helpTypes),
+                gender = userRequest.gender,
+                profession = userRequest.profession,
+                email = userRequest.email,
+                password = userRequest.password,
+                phone = userRequest.phone,
+                isHelper = userRequest.isHelper,
+                birth = userRequest.birth,
+                address = userRequest.address
+        )
+
+        return userRepository.save(user)
+    }
+
+    private fun convertToDatabaseColumn(attribute: List<HelpType>?): String? {
+        if (attribute != null && attribute.isNotEmpty()) {
+            var value = ""
+            for (helpType in attribute) value += helpType.name + ","
+            return value.dropLast(1)
+        }
+        return null
+    }
+}
