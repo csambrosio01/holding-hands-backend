f771fd11df7cf5f39f6b92e9d0950ed7ed43e110
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-11-26 00:25:55 -0300

    Add flywaydb lib to manage migartions

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.4.0"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+	id("org.flywaydb.flyway") version "7.3.0"
 	kotlin("jvm") version "1.4.10"
 	kotlin("plugin.spring") version "1.4.10"
 	kotlin("plugin.jpa") version "1.4.10"
