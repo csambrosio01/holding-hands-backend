c4ec7656679381eae1bdff8561d09c62c820ac96
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-11-26 18:11:05 -0300

    Move service logic to UserService, also add another catch in controller

---
+++ src/main/kotlin/com/usp/holdinghands/services/UserServiceImpl.kt
@@ -0,0 +1,37 @@
+package com.usp.holdinghands.services
+
+import com.usp.holdinghands.models.HelpType
+import com.usp.holdinghands.models.User
+import com.usp.holdinghands.models.UserRequest
+import com.usp.holdinghands.repositories.UserRepository
+import org.springframework.stereotype.Service
+
+@Service
+class UserServiceImpl(private val userRepository: UserRepository) : UserService {
+
+    override fun createUser(userRequest: UserRequest): User {
+        val user = User(
+                name = userRequest.name,
+                helpTypes = convertToDatabaseColumn(userRequest.helpTypes),
+                gender = userRequest.gender,
+                profession = userRequest.profession,
+                email = userRequest.email,
+                password = userRequest.password,
+                phone = userRequest.phone,
+                isHelper = userRequest.isHelper,
+                birth = userRequest.birth,
+                address = userRequest.address
+        )
+
+        return userRepository.save(user)
+    }
+
+    private fun convertToDatabaseColumn(attribute: List<HelpType>?): String? {
+        if (attribute != null && attribute.isNotEmpty()) {
+            var value = ""
+            for (helpType in attribute) value += helpType.name + ","
+            return value.dropLast(1)
+        }
+        return null
+    }
+}
