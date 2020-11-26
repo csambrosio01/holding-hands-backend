ba01bf22ff97e40eebbad72ff15f04178f5cde56
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-11-25 22:11:14 -0300

    Run generation script of spring

---
+++ src/test/kotlin/com/usp/holdinghands/HoldinghandsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.usp.holdinghands
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HoldinghandsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
