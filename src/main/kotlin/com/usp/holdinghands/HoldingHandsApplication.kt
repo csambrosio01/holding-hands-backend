238e15c12ba08dd3c1d167668763bbe47cee0887
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-11-26 22:03:00 -0300

    Add bean for PasswordEncoder

--- src/main/kotlin/com/usp/holdinghands/HoldingHandsApplication.kt
+++ src/main/kotlin/com/usp/holdinghands/HoldingHandsApplication.kt
@@ -2,9 +2,17 @@ package com.usp.holdinghands
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @SpringBootApplication
-class HoldingHandsApplication
+class HoldingHandsApplication {
+	@Bean
+	fun encoder(): PasswordEncoder? {
+		return BCryptPasswordEncoder()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<HoldingHandsApplication>(*args)
