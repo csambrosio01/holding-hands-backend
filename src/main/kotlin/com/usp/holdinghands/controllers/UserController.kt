59a3dbc3269a8494cff85311386fb32832224bef
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-11-26 22:02:14 -0300

    Change annotation to RestController

--- src/main/kotlin/com/usp/holdinghands/controllers/UserController.kt
+++ src/main/kotlin/com/usp/holdinghands/controllers/UserController.kt
@@ -5,13 +5,13 @@ import com.usp.holdinghands.services.UserService
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 @RequestMapping("/api/user")
 class UserController(val userService: UserService) {
 
