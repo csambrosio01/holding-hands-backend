3a6f9a9952eebe8d14ef97b9d5d7d0ea7a4fe1aa
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-12-07 20:23:58 -0300

    Add endpoint to get user history (all received, except ones that are Pending, and sent matchs) or matchs that user received (only pendings)

--- src/main/kotlin/com/usp/holdinghands/controllers/MatchController.kt
+++ src/main/kotlin/com/usp/holdinghands/controllers/MatchController.kt
@@ -7,10 +7,8 @@ import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 @RestController
 @RequestMapping("/api/match")
@@ -38,6 +36,20 @@ class MatchController(val matchService: MatchService) {
         val authentication = SecurityContextHolder.getContext().authentication
         return try {
             ResponseEntity(matchService.acceptRejectInvite(authentication, matchId, MatchStatus.valueOf(status.toUpperCase())), HttpStatus.OK)
+        } catch (e: NoSuchElementException) {
+            ResponseEntity("User not found", HttpStatus.NOT_FOUND)
+        } catch (e: UserNotFoundException) {
+            ResponseEntity("User not found", HttpStatus.NOT_FOUND)
+        } catch (e: DataIntegrityViolationException) {
+            ResponseEntity(e.localizedMessage, HttpStatus.CONFLICT)
+        }
+    }
+
+    @GetMapping("/{status}")
+    fun getPendingHistory(@PathVariable("status") status: String): ResponseEntity<Any> {
+        val authentication = SecurityContextHolder.getContext().authentication
+        return try {
+            ResponseEntity(matchService.getPendingHistory(authentication, MatchStatus.valueOf(status.toUpperCase())), HttpStatus.OK)
         } catch (e: NoSuchElementException) {
             ResponseEntity("User not found", HttpStatus.NOT_FOUND)
         } catch (e: UserNotFoundException) {
