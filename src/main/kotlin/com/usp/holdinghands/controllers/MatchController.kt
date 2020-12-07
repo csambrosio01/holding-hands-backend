15d34bd0d9e6597afe7a8558f49e9530b5d81e79
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-12-07 19:06:56 -0300

    Create endpoint to accept or reject a Match

--- src/main/kotlin/com/usp/holdinghands/controllers/MatchController.kt
+++ src/main/kotlin/com/usp/holdinghands/controllers/MatchController.kt
@@ -1,6 +1,7 @@
 package com.usp.holdinghands.controllers
 
 import com.usp.holdinghands.exceptions.UserNotFoundException
+import com.usp.holdinghands.models.MatchStatus
 import com.usp.holdinghands.services.MatchService
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.http.HttpStatus
@@ -20,6 +21,23 @@ class MatchController(val matchService: MatchService) {
         val authentication = SecurityContextHolder.getContext().authentication
         return try {
             ResponseEntity(matchService.sendInvite(authentication, userIdReceived), HttpStatus.OK)
+        } catch (e: NoSuchElementException) {
+            ResponseEntity("User not found", HttpStatus.NOT_FOUND)
+        } catch (e: UserNotFoundException) {
+            ResponseEntity("User not found", HttpStatus.NOT_FOUND)
+        } catch (e: DataIntegrityViolationException) {
+            ResponseEntity(e.localizedMessage, HttpStatus.CONFLICT)
+        }
+    }
+
+    @PostMapping("/{status}/{match_id_sent}")
+    fun acceptRejectInvitation(
+        @PathVariable("status") status: String,
+        @PathVariable("match_id_sent") matchId: Long
+    ): ResponseEntity<Any> {
+        val authentication = SecurityContextHolder.getContext().authentication
+        return try {
+            ResponseEntity(matchService.acceptRejectInvite(authentication, matchId, MatchStatus.valueOf(status.toUpperCase())), HttpStatus.OK)
         } catch (e: NoSuchElementException) {
             ResponseEntity("User not found", HttpStatus.NOT_FOUND)
         } catch (e: UserNotFoundException) {
